81012b7d05561e05e7fee57b70942dd2a39ee7da
saroyangor <dr.saroyan98@gmail.com>
2024-09-02 18:18:15 +0400

    hover on gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -4,14 +4,35 @@ import gallery3 from '../assets/gallery3.png';
 import gallery4 from '../assets/gallery4.png';
 import gallery5 from '../assets/gallery5.png';
 
+const firstLine = [
+	{ src: gallery1, title: 'New Generation' },
+	{ src: gallery2, title: 'New Generation' },
+];
+const secondLine = [
+	{ src: gallery3, title: 'New Generation' },
+	{ src: gallery4, title: 'New Generation' },
+	{ src: gallery5, title: 'New Generation' },
+];
+
 const Gallery = (): JSX.Element => {
 	return (
 		<article className="my-16 grid grid-cols-6">
-			<img src={gallery1} alt="gallery image 1" className="col-span-3" />
-			<img src={gallery2} alt="gallery image 2" className="col-span-3" />
-			<img src={gallery3} alt="gallery image 3" className="col-span-2" />
-			<img src={gallery4} alt="gallery image 4" className="col-span-2" />
-			<img src={gallery5} alt="gallery image 5" className="col-span-2" />
+			{firstLine.map((image) => (
+				<div key={image.src} className="gallery-item col-span-3 relative">
+					<img src={image.src} alt={image.title} />
+					<h3 className="absolute text-white text-[64px] bottom-1 left-1/2 translate-x-[-50%] whitespace-nowrap text-center hidden">
+						{image.title}
+					</h3>
+				</div>
+			))}
+			{secondLine.map((image) => (
+				<div key={image.src} className="gallery-item col-span-2 relative">
+					<img src={image.src} alt={image.title} />
+					<h3 className="absolute text-white text-[64px] bottom-1 left-1/2 translate-x-[-50%] whitespace-nowrap text-center hidden">
+						{image.title}
+					</h3>
+				</div>
+			))}
 		</article>
 	);
 };
