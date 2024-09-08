b6f79f2befc8cff45bdcc7d837e2546806ad8427
saroyangor <dr.saroyan98@gmail.com>
2024-09-08 20:37:17 +0400

    fix assets

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -11,7 +11,7 @@ const Gallery = (): JSX.Element => {
 			{firstLine.map((image) => (
 				<div key={image.src} className="gallery-item col-span-3 relative">
 					<Link to={`/media/${image.id}`}>
-						<img src={require(image.src)} alt={image.title} />
+						<img src={image.src} alt={image.title} />
 						<h3 className="absolute text-white text-[64px] bottom-1 left-1/2 translate-x-[-50%] whitespace-nowrap text-center hidden">
 							{image.title}
 						</h3>
